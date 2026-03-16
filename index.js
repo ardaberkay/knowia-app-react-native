72d57176630ab80a6c244c413567092aa171007c
Arda Berkay Bağım <ardabeb@hotmail.com>
2026-03-16 20:58:06 +0300

    added buffer

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 
+import { Buffer } from 'buffer';
+global.Buffer = Buffer;
+
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
